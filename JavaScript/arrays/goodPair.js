09ccab34d51d87eb570afe12732599f59b9f2d4b
Abhinav Alok <abhinav@liquide.life>
2023-08-22 16:04:03 +0530

    Refactors code

--- JavaScript/arrays/goodPair.js
+++ JavaScript/arrays/goodPair.js
@@ -1,11 +1,12 @@
 /**
  *  Problem Description:
-            Given an array A and an integer B. A pair(i, j) in the array is a good pair if i != j and (A[i] + A[j] == B). Check if any good pair exist or not.
+            Given an array A and an integer B. A pair(i, j) 
+            in the array is a good pair if i != j and (A[i] + A[j] == B). 
+            Check if any good pair exist or not.
 
     Problem Constraints:
             1 <= A.size() <= 104
             1 <= A[i] <= 109
-
             1 <= B <= 109
 
     Input Format
