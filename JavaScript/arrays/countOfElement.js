2df916fe463b38b800e58eba330451c5b45c226f
Abhinav Alok <abhinav@liquide.life>
2023-08-22 15:47:37 +0530

    Adds solution for Count of Elements

---
+++ JavaScript/arrays/countOfElement.js
@@ -0,0 +1,43 @@
+/**
+ *  Problem Description:   
+ *          Given an array A of N integers. 
+            Count the number of elements that have at least 1 elements greater than itself.
+
+    Problem Constraints:
+            1 <= N <= 105
+            1 <= A[i] <= 109
+
+    Input Format:
+            First and only argument is an array of integers A.
+
+    Output Format:
+            Return the count of elements.
+
+ */
+
+// Solution:
+
+function countOfElement(A){
+    let maxElement = 0
+    for(let i=0; i<=A.length-1; i++){
+        if(A[i]> maxElement){
+            maxElement = A[i]
+        }
+    }
+    let countOfMaxElement = 0;
+    for(let i=0; i<=A.length-1; i++){
+        if(A[i] === maxElement){
+            countOfMaxElement ++
+        }
+    }
+    let ans = A.length - countOfMaxElement;
+    return ans
+}
+
+// Please uncomment the function call for which you want to execute the code.
+
+// Function Call 1:
+// countOfElement([3, 1, 2])
+
+// Function Call 2:
+// countOfElement([5, 5, 3])
