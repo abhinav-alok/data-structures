fa618f95507d69c6c9b8056bc037db0878701511
Abhinav Alok <abhinav@liquide.life>
2023-08-24 01:24:27 +0530

    Adds solution for second largest item in array

---
+++ TypeScript/arrays/easy/secondLargest.ts
@@ -0,0 +1,44 @@
+/**
+*   You are given an integer array array. 
+    You have to find the second largest element/value in the array or report that no such element exists.
+
+    Problem Constraints:
+            1 <= |array| <= 105
+            0 <= array[i] <= 109
+
+    Input Format
+            The first argument is an integer array array.
+
+    Output Format
+            Return the second largest element. If no such element exist then return -1.
+
+*/
+
+// Solution:
+
+function secondLargestElement(array: number[]) : number{
+    if(array.length === 1){
+        return -1
+    }
+    let largest = 0
+    let secondLargest = -1
+    for(let i=0; i<=array.length-1; i++){
+        if(array[i] >=  largest){
+            largest = array[i]
+        }
+    }
+    for(let i=0; i<=array.length-1; i++){
+        if(array[i] > secondLargest && largest > array[i]){
+            secondLargest = array[i]
+        }
+    }
+    return Number(secondLargest)
+}
+
+// Please uncomment the function call for which you want to execute the code.
+
+// Function Call 1:
+// console.log(secondLargestElement([2, 1, 2]))
+
+// Function Call 2:
+console.log(secondLargestElement([2]))
